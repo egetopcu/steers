f242dab6cbd8a0543358591fc21a8ca4358b0735
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-11-08 19:03:25 +0100

    generated files and style changes

--- app/src/components/SelectTopics.svelte
+++ app/src/components/SelectTopics.svelte
@@ -1,67 +1,65 @@
 <script lang="ts">
-    import type { TopicData } from "@steers/common";
-    import differenceBy from "lodash/differenceBy";
-    import Debug from "./Debug.svelte";
+  import type { TopicData } from "@steers/common";
+  import differenceBy from "lodash/differenceBy";
+  import Debug from "./Debug.svelte";
 
-    export let selected: TopicData[];
-    export let topics: TopicData[];
-    export let loading: boolean = false;
+  export let selected: TopicData[];
+  export let topics: TopicData[];
+  export let loading: boolean = false;
 
-    let suggestions: TopicData[];
-    $: suggestions = differenceBy(topics, selected, "id");
+  let suggestions: TopicData[];
+  $: suggestions = differenceBy(topics, selected, "id");
 
-    const toggleTopicFunction = (topic: TopicData) => {
-        return (ev) => {
-            ev.preventDefault();
-            if (selected.find((cur) => cur.id === topic.id)) {
-                // remove topic
-                selected = selected.filter((cur) => cur.id !== topic.id);
-            } else {
-                // add to topics
-                selected = [...selected, topic];
-            }
-        };
+  const toggleTopicFunction = (topic: TopicData) => {
+    return (ev) => {
+      ev.preventDefault();
+      if (selected.find((cur) => cur.id === topic.id)) {
+        // remove topic
+        selected = selected.filter((cur) => cur.id !== topic.id);
+      } else {
+        // add to topics
+        selected = [...selected, topic];
+      }
     };
+  };
 </script>
 
 <div class="select-topics">
-    <div class="topics" class:loading>
-        {#each selected as topic}
-            <button
-                class="topic button is-primary is-small"
-                on:click={toggleTopicFunction(topic)}
-                on:keypress={toggleTopicFunction(topic)}
-                disabled={loading ? true : null}
-            >
-                {topic.name}
-            </button>
-        {/each}
-        {#each suggestions as topic}
-            <button
-                class="topic button is-small"
-                class:is-primary={selected.find((cur) => cur.id === topic.id)}
-                on:click={toggleTopicFunction(topic)}
-                on:keypress={toggleTopicFunction(topic)}
-                disabled={loading ? true : null}
-                class:is-loading={loading}
-            >
-                {topic.name}
-            </button>
-        {/each}
-    </div>
-    <Debug label="Topics" data={{ selected, choices: topics }} />
+  <div class="topics" class:loading>
+    {#each selected as topic}
+      <button
+        class="topic button is-primary is-small"
+        on:click={toggleTopicFunction(topic)}
+        on:keypress={toggleTopicFunction(topic)}
+        disabled={loading ? true : null}>
+        {topic.name}
+      </button>
+    {/each}
+    {#each suggestions as topic}
+      <button
+        class="topic button is-small"
+        class:is-primary={selected.find((cur) => cur.id === topic.id)}
+        on:click={toggleTopicFunction(topic)}
+        on:keypress={toggleTopicFunction(topic)}
+        disabled={loading ? true : null}
+        class:is-loading={loading}>
+        {topic.name}
+      </button>
+    {/each}
+  </div>
+  <Debug label="Topics" data={{ selected, choices: topics }} />
 </div>
 
 <style lang="less">
-    .topics {
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: flex-start;
-        align-items: flex-start;
-        gap: 0.2em;
-    }
+  .topics {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 0.2em;
+  }
 
-    .topic {
-        cursor: pointer;
-    }
+  .topic {
+    cursor: pointer;
+  }
 </style>
