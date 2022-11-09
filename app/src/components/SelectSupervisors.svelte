a3e2fa765a19bff8fe5829b4038977a231596359
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-11-09 23:48:44 +0100

    continue on supervisor list

--- app/src/components/SelectSupervisors.svelte
+++ app/src/components/SelectSupervisors.svelte
@@ -1,28 +1,54 @@
 <script lang="ts">
-  import { getTutors } from "../utils/api";
-  import { IQuery, query } from "../stores";
-  import differenceBy from "lodash/differenceBy";
-  import type { TutorData } from "@steers/common";
-  import Debug from "./Debug.svelte";
-  import Supervisor from "./Supervisor.svelte";
+    import { getTutors } from "../utils/api";
+    import { IQuery, query } from "../stores";
+    import differenceBy from "lodash/differenceBy";
+    import type { TutorData } from "@steers/common";
+    import Supervisor from "./Supervisor.svelte";
+    import { debounce } from "lodash";
 
-  let supervisors: TutorData[];
-  let filter: string = "";
+    let choices: TutorData[] = [];
+    let filter: string = "";
 
-  let suggestions: TutorData[] = [];
-  $: suggestions = differenceBy(supervisors, $query.tutors, "id");
+    let suggestions: TutorData[] = [];
+    $: suggestions = differenceBy(choices, $query.tutors, "id").slice(0, 10);
 
-  async function updateChoices(query: IQuery) {
-    supervisors = await getTutors(filter, query);
-  }
-  $: updateChoices($query);
+    async function updateChoices(filter: string, query: IQuery) {
+        choices = await getTutors({
+            filter,
+            required: {},
+            optional: {
+                programme: query.programme?.id,
+                categories: query.categories?.map((c) => c.id),
+                topics: query.topics?.map((t) => t.id),
+                tutors: query.tutors?.map((t) => t.id),
+                clients: query.clients?.map((t) => t.id),
+            },
+        });
+    }
+    const debouncedUpdate = debounce(updateChoices, 1000, {
+        maxWait: 5000,
+        leading: false,
+        trailing: true,
+    });
+
+    $: debouncedUpdate(filter, $query);
 </script>
 
 <div class="select-supervisors">
-  {#each $query.tutors as supervisor}
-    <Supervisor {supervisor} />
-  {/each}
-  {#each suggestions as supervisor}
-    <Supervisor {supervisor} />
-  {/each}
+    <input bind:value={filter} type="text" />
+    {#if $query.tutors?.length}
+        {#each $query.tutors as supervisor}
+            <Supervisor {supervisor} />
+        {/each}
+    {/if}
+    {#each suggestions as supervisor}
+        <Supervisor {supervisor} />
+    {/each}
 </div>
+
+<style lang="less">
+    .select-supervisors {
+        display: flex;
+        flex-flow: column;
+    }
+</style>
--- app/src/components/Supervisor.svelte
+++ app/src/components/Supervisor.svelte
@@ -1,44 +1,133 @@
 <script lang="ts">
-  import type { CategoryData, TutorData } from "@steers/common";
-  import { getCategories } from "../utils/api";
-  import Debug from "./Debug.svelte";
+    import type {
+        CategoryData,
+        ClientData,
+        TopicData,
+        TutorData,
+    } from "@steers/common";
+    import { IQuery, query } from "../stores";
+    import { getCategories, getClients, getTopics } from "../utils/api";
+    import { view } from "../utils/view";
+    import Debug from "./Debug.svelte";
 
-  export let supervisor: TutorData;
+    export let supervisor: TutorData;
+    const _query = view(query, ($query) => $query);
 
-  let details: any;
-  $: fetchDetails(supervisor);
-  async function fetchDetails(supervisor: TutorData) {
-    let response = await fetch(
-      `https://people.utwente.nl/peoplepagesopenapi/contacts?query=${encodeURIComponent(
-        supervisor.mail
-      )}`
-    );
-    let data = await response.json();
-    if (data?.data && data.data.length >= 1) {
-      details = data.data[0];
+    let details: any;
+    let related_categories: CategoryData[] = [];
+    let related_topics: TopicData[] = [];
+    let related_clients: ClientData[] = [];
+
+    $: update_people_details(supervisor);
+    $: update_related_categories(supervisor, $query);
+    $: update_related_clients(supervisor, $query);
+    $: update_related_topics(supervisor, $query);
+
+    async function update_people_details(tutor: TutorData) {
+        let response = await fetch(
+            `https://people.utwente.nl/peoplepagesopenapi/contacts?query=${encodeURIComponent(
+                tutor.mail
+            )}`
+        );
+        let data = await response.json();
+        if (data?.data && data.data.length >= 1) {
+            details = data.data[0];
+        }
     }
-  }
 
-  let interests: CategoryData[] = [];
-  $: fetchInterests(supervisor);
-  async function fetchInterests(supervisor: TutorData) {
-    interests = await getCategories("", { tutors: [supervisor] });
-  }
+    async function update_related_categories(tutor: TutorData, query: IQuery) {
+        related_categories = await getCategories({
+            limit: 3,
+            required: {
+                tutors: [tutor.id],
+            },
+            optional: {
+                programme: query.programme?.id,
+                categories: query.categories?.map((c) => c.id),
+            },
+        });
+    }
+
+    async function update_related_topics(tutor: TutorData, query: IQuery) {
+        related_topics = await getTopics({
+            limit: 3,
+            required: {
+                tutors: [tutor.id],
+            },
+            optional: {
+                programme: query.programme?.id,
+                categories: query.categories?.map((c) => c.id),
+            },
+        });
+    }
+
+    async function update_related_clients(tutor: TutorData, query: IQuery) {
+        related_clients = await getClients({
+            limit: 3,
+            required: {
+                tutors: [tutor.id],
+            },
+            optional: {
+                programme: query.programme?.id,
+                categories: query.categories?.map((c) => c.id),
+            },
+        });
+    }
 </script>
 
 <div class="supervisor">
-  <div class="name">
-    {details?.name || supervisor.name} ({details?.givenName})
-  </div>
-  {#if details}
+    <div class="details">
+        <div class="name">
+            {details?.name || supervisor.name} ({details?.givenName})
+        </div>
+        <div class="contact">
+            {#if supervisor.mail != "NA"}
+                <a href="mailto:{supervisor.mail}">{supervisor.mail}</a>
+            {/if}
+        </div>
+        <Debug
+            label="details"
+            data={{
+                supervisor,
+                details,
+                related_categories,
+                related_clients,
+                related_topics,
+            }}
+        />
+    </div>
     <div class="department">
-      {details?.organizations.map((org) => org.organizationId).join(", ")}
+        {details?.organizations.map((org) => org.organizationId).join(", ")}
     </div>
-  {/if}
-  {#if supervisor.mail != "NA"}
-    <div class="contact">
-      <a href="mailto:{supervisor.mail}">{supervisor.mail}</a>
+
+    <div class="interests">
+        {#if related_categories?.length}
+            {#each related_categories as interest}
+                <div class="interest">{interest.name}</div>
+            {/each}
+        {/if}
     </div>
-  {/if}
-  <Debug label="details" data={{ supervisor, details }} />
+    <div class="topics">
+        {#if related_topics?.length}
+            {#each related_topics as topic}
+                <div class="topic">{topic.name}</div>
+            {/each}
+        {/if}
+    </div>
+    <div class="clients">
+        {#if related_clients?.length}
+            {#each related_clients as client}
+                <div class="client">{client.name}</div>
+            {/each}
+        {/if}
+    </div>
 </div>
+
+<style>
+    .supervisor {
+        display: grid;
+
+        grid-template-rows: min-content;
+        grid-template-columns: 4fr 1fr repeat(3, 3fr);
+    }
+</style>
