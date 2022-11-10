a40387471654cd1ce6b660c62f3ff1a3455d90d0
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-11-10 15:35:23 +0100

    refactor api helpers into single function

--- app/src/utils/api.ts
+++ app/src/utils/api.ts
@@ -10,52 +10,36 @@ import type {
 
 const API_BASE = "http://localhost:3000";
 
-export async function getCategories(query: QueryData): Promise<CategoryData[]> {
-    const url = new URL("categories", API_BASE);
-    console.log(query);
+async function getResource(path: string, query: QueryData): Promise<any[]> {
+    const url = new URL(path, API_BASE);
+    console.log({ resource: path, url: url.toString(), query });
     return await fetch(url, {
         method: "POST",
         body: JSON.stringify(query),
         headers: { "Content-Type": "application/json" },
     }).then((result) => result.json());
+}
+
+export async function getCategories(query: QueryData): Promise<CategoryData[]> {
+    return getResource("categories", query);
 }
 
 export async function getProgrammes(
     query: QueryData
 ): Promise<ProgrammeData[]> {
-    const url = new URL("programmes", API_BASE);
-    return await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(query),
-        headers: { "Content-Type": "application/json" },
-    }).then((result) => result.json());
+    return getResource("programmes", query);
 }
 
 export async function getClients(query: QueryData): Promise<ClientData[]> {
-    const url = new URL("clients", API_BASE);
-    return await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(query),
-        headers: { "Content-Type": "application/json" },
-    }).then((result) => result.json());
+    return getResource("clients", query);
 }
 
 export async function getTutors(query: QueryData): Promise<TutorData[]> {
-    const url = new URL("tutors", API_BASE);
-    return await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(query),
-        headers: { "Content-Type": "application/json" },
-    }).then((result) => result.json());
+    return getResource("tutors", query);
 }
 
 export async function getTopics(query: QueryData): Promise<TopicData[]> {
-    const url = new URL("topics", API_BASE);
-    return await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(query),
-        headers: { "Content-Type": "application/json" },
-    }).then((result) => result.json());
+    return getResource("topics", query);
 }
 
 export async function getEssays(query: QueryData): Promise<
@@ -68,10 +52,5 @@ export async function getEssays(query: QueryData): Promise<
             tutors: TutorData[];
         }>)[]
 > {
-    const url = new URL("essays", API_BASE);
-    return await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(query),
-        headers: { "Content-Type": "application/json" },
-    }).then((result) => result.json());
+    return getResource("essays", query);
 }
