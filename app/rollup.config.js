1d3ab74ba36587a1efbd898e9deadfab4cc95cf1
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2023-01-23 10:26:30 +0100

    [svelte] ignore warnings in dependencies

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -43,7 +43,13 @@ export default {
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
 				// enable run-time checks when not in production
-				dev: !production
+				dev: !production,
+
+			},
+			onwarn: (warning, handler) => {
+				// ignore warnings raised in dependencies
+				if (warning["filename"]?.includes("\\node_modules\\")) return;
+				handler(warning);
 			}
 		}),
 		// we'll extract any component CSS out into
