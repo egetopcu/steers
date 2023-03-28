21c0811dfad0f576426d189b92b1ebe06be786ca
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2023-03-28 10:49:00 +0200

    move category selection to table style widget

---
+++ app/src/components/SelectCategories.v2.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+    import differenceBy from "lodash/differenceBy";
+    import debounce from "lodash/debounce";
+
+    import type { CategoryData } from "@steers/common";
+    import { getCategories } from "../utils/api";
+    import { IQuery, query } from "../stores";
+
+    import Paginator from "./utility/Paginator.svelte";
+    import Category from "./Category.svelte";
+    import StarButton from "./StarButton.svelte";
+
+    let filter: string = "";
+    let page = 1;
+    let choices: CategoryData[] = [];
+    let available: CategoryData[] = [];
+    $: available = differenceBy(choices, $query.categories, "id");
+
+    let suggestions: CategoryData[] = [];
+    $: suggestions = available.slice((page - 1) * 10, page * 10);
+
+    let pages: number;
+    $: pages = Math.ceil(available.length / 10);
+
+    async function updateChoices(filter: string, query: IQuery) {
+        choices = await getCategories({
+            filter,
+            required: {
+                programme: query.programme?.id,
+                categories: query.categories?.map((c) => c.id),
+            },
+            optional: {
+                topics: query.topics?.map((t) => t.id),
+                tutors: query.tutors?.map((t) => t.id),
+                clients: query.clients?.map((t) => t.id),
+            },
+            // sort: "similarity DESC",
+        });
+    }
+
+    const debouncedUpdate = debounce(updateChoices, 1000, {
+        maxWait: 5000,
+        leading: false,
+        trailing: true,
+    });
+    $: debouncedUpdate(filter, $query);
+
+    function addCategory(category: CategoryData) {
+        $query.categories = [...($query.categories ?? []), category];
+    }
+
+    function removeCategory(category: CategoryData) {
+        $query.categories = $query.categories.filter(
+            (_category) => _category.id !== category.id
+        );
+    }
+</script>
+
+<div class="select-categories">
+    {#if $query.categories?.length}
+        {#each $query.categories as category}
+            <Category {category}>
+                <StarButton
+                    slot="details-extra"
+                    label="Remove from search"
+                    on:click={() => removeCategory(category)}
+                    active={true}
+                />
+            </Category>
+        {/each}
+    {/if}
+    <input bind:value={filter} type="text" />
+    <Paginator bind:page max_page={pages} />
+    <div class="category-table">
+        {#each suggestions as category}
+            <Category {category}>
+                <StarButton
+                    slot="details-extra"
+                    label="Add to search"
+                    on:click={() => addCategory(category)}
+                />
+            </Category>
+        {/each}
+    </div>
+    <Paginator bind:page max_page={pages} />
+</div>
+
+<style lang="less">
+    .select-categories {
+        display: flex;
+        flex-flow: column;
+    }
+</style>
